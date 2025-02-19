cffffe3272c2d18f2733331cafbc49f2cc4246f7
Omar El Garwany <oe2@sanger.ac.uk>
2025-02-19 10:58:38 +0000

    Fix mtx script to concatenate multiple IsoQuant counts files properly

--- scripts/convert_linear_counts_to_mtx.py
+++ scripts/convert_linear_counts_to_mtx.py
@@ -2,9 +2,8 @@ import pandas as pd
 import argparse
 
 
-def create_mtx(isoquant_f):
-    isoquant_counts_dat=pd.read_csv(isoquant_f,sep='\t')
-    print('Loaded isoquant (linear) counts...')
+def create_mtx(isoquant_counts_dat):
+
 
     #Extracting feature index
     features_dat=isoquant_counts_dat['#feature_id'].drop_duplicates().reset_index(drop=True).reset_index().rename(columns={'index':'feature_idx'})
@@ -24,7 +23,7 @@ def create_mtx(isoquant_f):
     full_mtx=full_mtx.loc[full_mtx['count']!=0,:]
     print('Filtered out 0 counts...')
 
-    return [features_dat,groups_dat,full_mtx]
+    return [full_mtx,features_dat.drop(columns=['feature_idx']),groups_dat.drop(columns=['group_idx'])]
 
 
 
@@ -36,11 +35,13 @@ def create_mtx(isoquant_f):
 
 
 def write_mtx_dat(full_mtx,features_dat,groups_dat,output_dir,output_prefix):
+        if output_prefix.strip() != '':
+            output_prefix=output_prefix+'.'
         #First line data
         firstline_dat=' '.join([str(features_dat.shape[0]), str(groups_dat.shape[0]),str(full_mtx.shape[0])])+'\n'
 
         #Headers and mtx file
-        full_mtx_f=open(output_dir+'matrix.mtx','w')
+        full_mtx_f=open(output_dir+output_prefix+'matrix.mtx','w')
         full_mtx_f.write("%%MatrixMarket matrix coordinate real general\n")
         full_mtx_f.write("%\n")
         full_mtx_f.write(firstline_dat)
@@ -59,13 +60,22 @@ def write_mtx_dat(full_mtx,features_dat,groups_dat,output_dir,output_prefix):
         print('wrote barcodes.tsv file...')
         print('==================')
 
+def load_all_isoquant_dat(isoquant_count_fs):
+    all_isoqunat_counts_dat=pd.DataFrame()
+    for isoquant_count_f in isoquant_count_fs:
+        isoquant_counts_dat=pd.read_csv(isoquant_count_f,sep='\t')
+        isoquant_counts_dat=isoquant_counts_dat.loc[isoquant_counts_dat['count']!=0,:]
+        all_isoqunat_counts_dat=pd.concat([all_isoqunat_counts_dat,isoquant_counts_dat],ignore_index=True)
+        print('Loaded isoquant (linear) counts from {f}...'.format(f=isoquant_count_f))
+    return all_isoqunat_counts_dat
+
 
 def main():
     parser = argparse.ArgumentParser(description="Process Isoquant count file with specified parameters.")
 
     parser.add_argument("-i",'--isoquant_files', dest='isoquant_count_fs',nargs='+',required=True,type=str, help="Path to the Isoquant count file.")
-    parser.add_argument('-d',"--output_dir", dest='output_dir',required=True,type=str, help="Directory where output files will be saved.")
-    parser.add_argument('-p',"--output_prefix", dest='output_prefix',required=True,type=str, help="Prefix for output file names.")
+    parser.add_argument('-d',"--output_dir", dest='output_dir',default='',type=str, help="Directory where output files will be saved.")
+    parser.add_argument('-p',"--output_prefix", dest='output_prefix',default='',type=str, help="Prefix for output file names.")
 
     args = parser.parse_args()
 
@@ -74,12 +84,23 @@ def main():
     all_features_dat=pd.DataFrame()
     all_groups_dat=pd.DataFrame()
 
-    for isoquant_f in args.isoquant_fs
-        full_mtx,features_dat,groups_dat=create_mtx(isoquant_f)
-        all_mtx_dat=pd.concat([all_mtx_dat,full_mtx],ignore_index=True)
-        all_features_dat=pd.concat([all_features_dat,features_dat],ignore_index=True)
-        all_groups_dat=pd.concat([all_groups_dat,groups_dat],ignore_index=True)
+    output_prefix=args.output_prefix
+    output_dir=args.output_dir
+    isoquant_count_fs=args.isoquant_count_fs
 
-    write_mtx_dat(all_mtx_dat,all_features_dat,all_groups_dat,args.output_dir,args.output_prefix)
+
+
+    all_isoqunat_counts_dat=load_all_isoquant_dat(isoquant_count_fs)
+    print('=========')
+    print('Loaded all isoquant files')
+    print('=========')
+    full_mtx,features_dat,groups_dat=create_mtx(all_isoqunat_counts_dat)
+    print('=========')
+    print('Created MTX files')
+    print('=========')
+    write_mtx_dat(full_mtx,features_dat,groups_dat,output_dir,output_prefix)
+    print('=========')
+    print('Wrote MTX files')
+    print('=========')
 if __name__ == "__main__":
     main()
